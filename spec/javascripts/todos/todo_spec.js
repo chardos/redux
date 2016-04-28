10f92b3a030746a152439c9b7e4d23a62feda2e8
Richard Tan <chardos@gmail.com>
2016-04-28 23:26:51 +1000

    Todo toggle

--- spec/javascripts/todos/todo_spec.js
+++ spec/javascripts/todos/todo_spec.js
@@ -24,4 +24,26 @@ describe("Todos", function() {
     ).toEqual(stateAfter)
   });
 
+  it('should be able to toggle a todo',
+  function(){
+    var stateBefore = [
+      {id: 0, text: 'Learn redux', completed: false},
+      {id: 1, text: 'Go shopping', completed: false}
+    ]
+    var action = {
+      type: 'TOGGLE_TODO',
+      id: 1
+    }
+    var stateAfter = [
+      {id: 0, text: 'Learn redux', completed: false},
+      {id: 1, text: 'Go shopping', completed: true}
+    ]
+    Object.freeze(stateBefore);
+    Object.freeze(action);
+
+    expect(
+      todos(stateBefore, action)
+    ).toEqual(stateAfter)
+  })
+
 });
